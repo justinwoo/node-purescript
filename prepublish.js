5cc2aee2081a86ec807d1c07f865d48005b0eca7
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-05-28 01:01:25 +0900

    update dependencies and devDependencies
    Highlights:
    * use async-await and object spread properties https://github.com/shinnn/install-purescript/commit/61613a4a61f3c70861b6f878b056d69f950428f7
    * read the default binary name from package.json in the target directory https://github.com/shinnn/install-purescript-cli/commit/c6bd437a9ba98f87755098adab6716adfdd7e31e
    * support PureScript v0.12.0-rc1 https://github.com/shinnn/download-purescript/commit/e878c26358f1949879fb0aacdec917d264a93e6e
    * update @shinnn/eslint-config and follow its rules, for exmaple tab indent
    * remove some dev dependencies no longer needed for Node.js >= 8
    
    [BREAKING] drop support for Node.js < 8

--- prepublish.js
+++ prepublish.js
@@ -2,7 +2,6 @@
 
 const {writeFileSync} = require('fs');
 const {join} = require('path');
-const values = require('lodash/fp/values');
 
 const {bin, name} = require('./package.json');
 const content = `This is a placeholder file of a PureScript binary installed with npm.
@@ -11,4 +10,4 @@ and the placeholder has not been replaced with a valid binary.
 Try installing \`${name}\` npm package again.
 `;
 
-writeFileSync(join(__dirname, values(bin)[0]), content, {mode: '755'});
+writeFileSync(join(__dirname, Object.values(bin)[0]), content, {mode: '755'});
--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 'use strict';
 
+const {lstat} = require('fs');
+const {promisify} = require('util');
 const {resolve} = require('path');
 
 const execa = require('execa');
-const lstat = require('lstat');
 const purescript = require('.');
 const semverRegex = require('semver-regex');
 const test = require('tape');
@@ -11,56 +12,56 @@ const test = require('tape');
 const {bin, scripts} = require('./package.json');
 
 test('`bin` field of package.json', t => {
-  t.deepEqual(
-    Object.keys(bin),
-    ['purs'],
-    'should only include a single path.'
-  );
+	t.deepEqual(
+		Object.keys(bin),
+		['purs'],
+		'should only include a single path.'
+	);
 
-  t.end();
+	t.end();
 });
 
 test('Node.js API', t => {
-  t.equal(
-    typeof purescript,
-    'string',
-    'should expose a string.'
-  );
+	t.equal(
+		typeof purescript,
+		'string',
+		'should expose a string.'
+	);
 
-  t.equal(
-    purescript,
-    resolve(bin.purs),
-    'should be equal to the binary path.'
-  );
+	t.equal(
+		purescript,
+		resolve(bin.purs),
+		'should be equal to the binary path.'
+	);
 
-  t.end();
+	t.end();
 });
 
 test('`prepublishOnly` script', async t => {
-  await execa('npm', ['run', 'prepublishOnly']);
-  const stat = await lstat(purescript);
+	await execa('npm', ['run', 'prepublishOnly']);
+	const stat = await promisify(lstat)(purescript);
 
-  t.ok(
-    stat.isFile(),
-    'should create a placeholder file.'
-  );
+	t.ok(
+		stat.isFile(),
+		'should create a placeholder file.'
+	);
 
-  t.ok(
-    stat.size < 250,
-    'should create a sufficiently small file.'
-  );
+	t.ok(
+		stat.size < 250,
+		'should create a sufficiently small file.'
+	);
 
-  t.end();
+	t.end();
 });
 
 test('`postinstall` script', async t => {
-  await execa('npm', ['run', 'postinstall']);
+	await execa('npm', ['run', 'postinstall']);
 
-  t.equal(
-    (await execa(purescript, ['--version'])).stdout,
-    scripts.postinstall.match(semverRegex())[0],
-    'should install a PureScript binary.'
-  );
+	t.equal(
+		(await execa(purescript, ['--version'])).stdout,
+		scripts.postinstall.match(semverRegex())[0],
+		'should install a PureScript binary.'
+	);
 
-  t.end();
+	t.end();
 });
